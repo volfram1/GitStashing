12d4e42b6f42db0a0f5725af9dc8700841c5da7e
volfram1 <atomczak256@gmail.com>
2022-05-18 22:19:20 -0400

    the red audi car is ready with basic feature

--- src/com/groupProjects/Main.java
+++ src/com/groupProjects/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        BasicFeatures car = new BasicFeatures();
+        BasicFeatures audi = new BasicFeatures();
+        audi.color="red";
+        audi.doors="4";
+        audi.engineCapacity="V8";
+        audi.headlights="blue";
+        audi.regNo="tuzik";
+
+
     }
 }
